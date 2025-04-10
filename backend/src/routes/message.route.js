ff0f983d269770d17aa780d06f5c71ed77ff79d8
kiruthivasan R <jagadeshdhashwin@gmail.com>
2025-04-10 17:53:34 +0530

    Update message.route.js

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -7,14 +7,8 @@ import {
 } from "../controllers/message.controller.js";
 
 const router = express.Router();
-
-// ✅ Route to get all users except current user (for sidebar display)
 router.get("/users", protectRoute, getUsersForSidebar);
-
-// ✅ Route to get all messages exchanged with a specific user
 router.get("/:id", protectRoute, getMessages);
-
-// ✅ Route to send a message (text/image) to a specific user
 router.post("/send/:id", protectRoute, sendMessages);
 
 export default router;
