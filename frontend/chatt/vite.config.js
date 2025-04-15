f56a2c3f5a6937ac95165215c93a3500c96c0a73
kiruthivasan R <jagadeshdhashwin@gmail.com>
2025-04-15 12:36:41 +0530

    Update vite.config.js

--- frontend/chatt/vite.config.js
+++ frontend/chatt/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // keep as '.' if index.html is in the same folder
   server: {
-    port: 5173
-  }
+    port: 5174
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 });
