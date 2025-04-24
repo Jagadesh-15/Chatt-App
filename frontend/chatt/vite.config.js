826ce5e78116ed2a9d3ff5a1be12837cd737a58c
kiruthivasan R <136573939+Jagadesh-15@users.noreply.github.com>
2025-04-24 11:07:57 +0530

    Update vite.config.js

--- frontend/chatt/vite.config.js
+++ frontend/chatt/vite.config.js
@@ -3,13 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: '.', // current directory
   plugins: [react()],
-  // root: '.', // keep as '.' if index.html is in the same folder
-  // server: {
-  //   port: 5174
-  // },
-  // build: {
-  //   outDir: 'dist',
-  //   emptyOutDir: true,
-  // },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 });
